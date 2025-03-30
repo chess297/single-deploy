dfecb0c7ead45ec622f4b01c26a3c66c26fafc77
chess <viky.chen@outlook.com>
2025-03-30 17:33:33 +0800

    feat: add mysql sql go connect mysql

--- services/golang/internal/config/config.go
+++ services/golang/internal/config/config.go
@@ -4,4 +4,7 @@ import "github.com/zeromicro/go-zero/rest"
 
 type Config struct {
 	rest.RestConf
+	DatabaseDsn string
+	// 是否开启 ping 接口创建随机用户，安全起见demo不处理
+	OpenPingCreate bool
 }
--- services/golang/internal/svc/service_context.go
+++ services/golang/internal/svc/service_context.go
@@ -2,14 +2,21 @@ package svc
 
 import (
 	"golang/internal/config"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config   config.Config
+	Database sqlx.SqlConn
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	// dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	database := sqlx.NewMysql(c.DatabaseDsn)
+	// database.Transact()
 	return &ServiceContext{
-		Config: c,
+		Config:   c,
+		Database: database,
 	}
 }
