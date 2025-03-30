8ae3817e5268534f6d18772af03fbfc3ce7f18c8
chess <viky.chen@outlook.com>
2025-03-30 13:04:12 +0800

    feat: add golang project

---
+++ services/golang/internal/logic/ping_logic.go
@@ -0,0 +1,28 @@
+package logic
+
+import (
+	"context"
+
+	"golang/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type PingLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewPingLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PingLogic {
+	return &PingLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *PingLogic) Ping() error {
+
+	return nil
+}
